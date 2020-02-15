d9ed3ed05ee468639f61ddda3c4ffc8e7b56a636
Artyom Davydov <fax1ty.ru@gmail.com>
2020-02-15 19:04:02 +0400

    Animated marker movement

--- src/android/com/eclipsesource/tabris/maps/MarkerHandler.kt
+++ src/android/com/eclipsesource/tabris/maps/MarkerHandler.kt
@@ -1,26 +1,59 @@
 package com.eclipsesource.tabris.maps
 
+import android.animation.ObjectAnimator
+import android.animation.TypeEvaluator
 import com.eclipsesource.tabris.android.ActivityScope
 import com.eclipsesource.tabris.android.ObjectHandler
 import com.eclipsesource.tabris.android.Property
+import com.eclipsesource.tabris.android.internal.ktx.toList
 import com.eclipsesource.v8.V8Object
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import java.lang.Exception
+
 
 @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
 class MarkerHandler(scope: ActivityScope) : ObjectHandler<MapMarker> {
 
-  override val type = "com.eclipsesource.maps.Marker"
+    override val type = "com.eclipsesource.maps.Marker"
 
-  override val properties: List<Property<MapMarker, *>> = listOf(
-      MarkerImageProperty(scope),
-      MarkerPositionProperty(),
-      MarkerSnippetProperty,
-      MarkerTitleProperty
-  )
+    override val properties: List<Property<MapMarker, *>> = listOf(
+            MarkerImageProperty(scope),
+            MarkerPositionProperty(),
+            MarkerSnippetProperty,
+            MarkerTitleProperty
+    )
 
-  override fun create(id: String, properties: V8Object) = MapMarker()
+    override fun call(mapMarker: MapMarker, method: String, properties: V8Object) = when (method) {
+        "moveTo" -> moveTo(mapMarker, properties)
+        // @fax1ty
+        else -> null
+    }
 
-  override fun destroy(mapMarker: MapMarker) {
-    mapMarker.marker?.remove()
-  }
+    private fun moveTo(mapMarker: MapMarker, properties: V8Object) {
+        val toPos = properties.getArray("to").toList<Double>()
+        val animate = properties.getBoolean("animate")
+        val duration = properties.getInteger("duration")
+        if (!animate) mapMarker.position = LatLng(toPos[0], toPos[1])
+        else {
+            MarkerAnimation.animateMarkerTo(mapMarker.marker, LatLng(toPos[0], toPos[1]), duration, LatLngInterpolator.Spherical())
+        }
+    }
 
+    override fun create(id: String, properties: V8Object) = MapMarker()
+
+    override fun destroy(mapMarker: MapMarker) {
+        mapMarker.marker?.remove()
+    }
+
+}
+
+object MarkerAnimation {
+    fun animateMarkerTo(marker: Marker?, finalPosition: LatLng, duration: Int, latLngInterpolator: LatLngInterpolator) {
+        val typeEvaluator = TypeEvaluator { fraction: Float, a: LatLng?, b: LatLng? -> latLngInterpolator.interpolate(fraction, a!!, b!!) }
+        val property = android.util.Property.of(Marker::class.java, LatLng::class.java, "position")
+        val animator = ObjectAnimator.ofObject(marker, property, typeEvaluator, finalPosition)
+        animator.duration = duration.toLong()
+        animator.start()
+    }
 }
--- www/Marker.js
+++ www/Marker.js
@@ -1,6 +1,6 @@
 var Marker = tabris.NativeObject.extend('com.eclipsesource.maps.Marker', tabris.Widget);
 
-Marker.prototype._listen = function(name, listening) {
+Marker.prototype._listen = function (name, listening) {
   if (name === 'tap') {
     this._nativeListen(name, listening);
   } else {
@@ -8,11 +8,17 @@ Marker.prototype._listen = function(name, listening) {
   }
 };
 
-Marker.prototype.dispose = function() {
+Marker.prototype.dispose = function () {
   if (this._map) {
     this._map.removeMarker(this);
   }
   this._dispose();
+};
+
+Marker.prototype.moveTo = function (values) {
+  if (!values.to) throw new Error(value + ' is not an array');
+  if (values.animate && !values.duration) throw new Error(value + ' is not an integer');
+  this._nativeCall('moveTo', values);
 };
 
 tabris.NativeObject.defineProperties(Marker.prototype, {
@@ -27,7 +33,7 @@ tabris.NativeObject.defineProperties(Marker.prototype, {
     },
     nocache: true
   },
-  image: {type: 'ImageValue', default: null}
+  image: { type: 'ImageValue', default: null }
 });
 
 module.exports = Marker;
